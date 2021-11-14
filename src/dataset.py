5b0591b54c9b6b3d97bfcf8f13f02d85e5acf60d
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-14 02:02:40 -0500

    encoding None -> utf-8

--- src/dataset.py
+++ src/dataset.py
@@ -61,7 +61,7 @@ class Caption:
     def get_data(self):
         return self._data
     def __read_raw_text(self):
-        with open(self._fpath, 'r', encoding=None) as file:
+        with open(self._fpath, 'r', encoding='utf-8') as file:
             text = file.read()
         return text
     def __set_data(self, data: List[str]):
