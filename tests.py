750ef1bcf74c417cfbb47084f725f32232dbb461
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-14 01:13:27 -0500

    -- trailing whitespace

--- tests.py
+++ tests.py
@@ -6,21 +6,21 @@ CAPTION_PATH = "datasets/testing/test.txt"
 c = Caption(CAPTION_PATH)
 
 read_output = [
-    'Go', '#follow', 'my', 
-    '#official', 'artwork', 'page', 
-    '(', '@rap_art', ')', 'finally', 
-    'finished', '#Artwork', '#Alert', 
-    '#workinprogress', '#50cent', 
-    '#RapArt', 'by', '#ShonWil', 
-    'on', '#Bristol', '#drawn', 
-    'with', '#prismacolor', 
-    '#prisma', '#marker', '#colorpencil', 
-    '#newyork', '#detroit', 
-    '#getrichordietryin', '#diamond', 
-    '#art', '#fresh', '#fly', '#dope', 
-    '#repost', '#tag', '#50cent', 
-    '#artist', '#music', '#hiphop', 
-    '#effen', '#effenvodka', '#power', 
+    'Go', '#follow', 'my',
+    '#official', 'artwork', 'page',
+    '(', '@rap_art', ')', 'finally',
+    'finished', '#Artwork', '#Alert',
+    '#workinprogress', '#50cent',
+    '#RapArt', 'by', '#ShonWil',
+    'on', '#Bristol', '#drawn',
+    'with', '#prismacolor',
+    '#prisma', '#marker', '#colorpencil',
+    '#newyork', '#detroit',
+    '#getrichordietryin', '#diamond',
+    '#art', '#fresh', '#fly', '#dope',
+    '#repost', '#tag', '#50cent',
+    '#artist', '#music', '#hiphop',
+    '#effen', '#effenvodka', '#power',
     'tag', '@50cent', 'Thanks!']
 clean_output = []
 ID_OUTPUT = "test"
