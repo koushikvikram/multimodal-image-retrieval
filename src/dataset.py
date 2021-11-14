82482d24be0f3f1d0621b0d26ed83b8b11ed6a1a
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-14 00:58:59 -0500

    ++ venv

--- src/dataset.py
+++ src/dataset.py
@@ -22,33 +22,55 @@ class Caption:
         self._fpath = filepath
         self._data = self.read()
         self._id = self.get_id()
+    
     def read(self):
         raw_text = self.__read_raw_text()
         words = raw_text.split(" ")
         self.__set_data(words)
+    
     def clean(self):
         text = self.__read_raw_text()
+        
         # remove non-ascii characters
         ascii_text = text.encode("ascii", "ignore").decode()
+        
         # remove non-alphanumeric characters
         pattern = re.compile('[^a-zA-Z\d\s]')
         alnum_text = pattern.sub('', ascii_text)
+        
         # lowercase the text
         lowercase_text = alnum_text.lower()
+        
         # remove stop words
         no_stop_words = remove_stopwords(lowercase_text)
+        
         # word tokenize
-        cleaned_words = word_tokenize(no_stop_words) 
-        self.__set_data(cleaned_words)
+        cleaned_words = word_tokenize(no_stop_words)
+        
+        # remove custom stopwords
+        custom_stopwords = ['http', 'https', 'photo', 'picture', 'image', 'insta', 'instagram', 'post']
+        clean_captions = []
+        for word in cleaned_words:
+            if word not in custom_stopwords:
+                clean_captions.append(word)
+
+        self.__set_data(clean_captions)
+    
+    def get_embeddings(self):
+        raise NotImplementedError
+
     def get_id(self):
         filename = self._fpath.split("/")[-1]
         id = filename.split(".")[0]
         return id
+    
     def get_data(self):
         return self._data
+    
     def __read_raw_text(self):
         with open(self._fpath, 'r') as f:
             text = f.read()
         return text
+    
     def __set_data(self, data: List[str]):
         self._data = data
