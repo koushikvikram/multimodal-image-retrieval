8282a01d3f740006549d5424890457f034a33f22
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-14 01:11:48 -0500

    list reformatted

--- tests.py
+++ tests.py
@@ -5,6 +5,22 @@ from src.dataset import Caption
 CAPTION_PATH = "datasets/testing/test.txt"
 c = Caption(CAPTION_PATH)
 
-read_output = ['Go', '#follow', 'my', '#official', 'artwork', 'page', '(', '@rap_art', ')', 'finally', 'finished', '#Artwork', '#Alert', '#workinprogress', '#50cent', '#RapArt', 'by', '#ShonWil', 'on', '#Bristol', '#drawn', 'with', '#prismacolor', '#prisma', '#marker', '#colorpencil', '#newyork', '#detroit', '#getrichordietryin', '#diamond', '#art', '#fresh', '#fly', '#dope', '#repost', '#tag', '#50cent', '#artist', '#music', '#hiphop', '#effen', '#effenvodka', '#power', 'tag', '@50cent', 'Thanks!']
+read_output = [
+    'Go', '#follow', 'my', 
+    '#official', 'artwork', 'page', 
+    '(', '@rap_art', ')', 'finally', 
+    'finished', '#Artwork', '#Alert', 
+    '#workinprogress', '#50cent', 
+    '#RapArt', 'by', '#ShonWil', 
+    'on', '#Bristol', '#drawn', 
+    'with', '#prismacolor', 
+    '#prisma', '#marker', '#colorpencil', 
+    '#newyork', '#detroit', 
+    '#getrichordietryin', '#diamond', 
+    '#art', '#fresh', '#fly', '#dope', 
+    '#repost', '#tag', '#50cent', 
+    '#artist', '#music', '#hiphop', 
+    '#effen', '#effenvodka', '#power', 
+    'tag', '@50cent', 'Thanks!']
 clean_output = []
-ID_output = "test"
+ID_OUTPUT = "test"
