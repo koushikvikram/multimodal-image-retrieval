5d3d3cc29747c853fa85f22918b9ebec62c9a310
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-14 23:44:31 -0500

    -> uppercase

--- model/train_word2vec.py
+++ model/train_word2vec.py
@@ -1,9 +1,8 @@
 '''Training script for Word2Vec on the dataset's captions'''
-import gensim
 from gensim.models import Word2Vec
 
 
-all_cleaned_captions = None
+ALL_CLEANED_CAPTIONS = None
 
-model = Word2Vec(all_cleaned_captions, min_count=5)
+model = Word2Vec(ALL_CLEANED_CAPTIONS, min_count=5)
 model.save("word2vec_instacities_default.model")
