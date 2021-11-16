9bd821082e0a5add878d9f731d247bac0b8a900e
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-15 22:20:29 -0500

    word2vec configs

--- config/word2vec.py
+++ config/word2vec.py
@@ -1,5 +1,9 @@
 """Word2Vec Configuration File"""
 
+from model.train_word2vec import CAPTIONS_PATH
+
+
+CAPTIONS_PATH = "/content/drive/MyDrive/InstaNY100K/captions/newyork/"
 SIZE = 100
 MIN_COUNT = 5
 N_CORES = 8
