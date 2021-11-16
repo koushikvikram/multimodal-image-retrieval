972b7615c4c61e5678fce16f718ff6c99ef6aad1
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-15 22:20:39 -0500

    moved to subdir

--- model/train_word2vec.py
+++ model/train_word2vec.py
@@ -1,10 +1,20 @@
 '''Training script for Word2Vec on the dataset's captions'''
+from src.dataset import Dataset
 from gensim.models import Word2Vec
 import config.word2vec as cfg
 
 
-WORD2VEC_DATASET = None
+# # prepare dataset for word2vec
+# dataset = Dataset(
+#     captions_path=cfg.CAPTIONS_PATH,
+#     images_path=""
+#     )
+# dataset.read_captions(clean=True)
 
+# we've already cleaned the captions
+WORD2VEC_DATASET = dataset.get_word2vec_dataset(min_count=cfg.MIN_COUNT)
+
+# train word2vec model
 model = Word2Vec(
     WORD2VEC_DATASET,
     size=cfg.SIZE,
