f1601e72cdf1a355f960ce72f436485ad6e02916
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-17 18:05:12 -0500

    -- import statement

--- config/word2vec.py
+++ config/word2vec.py
@@ -1,8 +1,5 @@
 """Word2Vec Configuration File"""
 
-from model.train_word2vec import CAPTIONS_PATH
-
-
 CAPTIONS_PATH = "/content/drive/MyDrive/InstaNY100K/captions/newyork/"
 SIZE = 100
 MIN_COUNT = 5
