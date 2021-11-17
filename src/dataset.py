2787e1f047e624d171d83e8f130a5fa7c4b47289
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-17 17:34:39 -0500

    Caption Class moved to separate file

--- src/dataset.py
+++ src/dataset.py
@@ -1,17 +1,10 @@
 '''Module for creating and working with the InstaNY100K dataset'''
-from typing import List
+from caption import Caption
+
 import glob
 from collections import Counter
 from tqdm import tqdm
 import pickle
-
-import re
-import nltk
-from gensim.parsing.preprocessing import remove_stopwords
-from nltk.tokenize import word_tokenize
-
-
-nltk.download('punkt')
 
 
 class EmptyDataset(Exception):
@@ -132,53 +125,3 @@ class Dataset:
     def __set_captions(self, captions):
         '''set self.captions_dataset to captions'''
         self.captions_dataset = captions
-
-class Caption:
-    '''Perform operations on individual caption textfiles'''
-    def __init__(self, filepath: str):
-        self._fpath = filepath
-        self._data = []
-        self._id = self.get_id()
-    def read(self, clean=False):
-        '''read textfile and make words from captions'''
-        text = self.__read_raw_text()
-        if clean:
-            # remove non-ascii characters
-            ascii_text = text.encode("ascii", "ignore").decode()
-            # remove non-alphanumeric characters
-            pattern = re.compile(r'[^a-zA-Z\d\s]')
-            alnum_text = pattern.sub('', ascii_text)
-            # lowercase the text
-            lowercase_text = alnum_text.lower()
-            # remove stop words
-            no_stop_words = remove_stopwords(lowercase_text)
-            # word tokenize
-            cleaned_words = word_tokenize(no_stop_words)
-            # remove custom stopwords
-            custom_stopwords = [
-                'http', 'https', 'photo', 'picture',
-                'image', 'insta', 'instagram', 'post']
-            clean_captions = []
-            for word in cleaned_words:
-                if word not in custom_stopwords:
-                    clean_captions.append(word)
-            self.__set_data(clean_captions)
-        else:
-            words = text.split(" ")
-            self.__set_data(words)
-    def get_id(self):
-        '''get caption's ID, present in filename'''
-        filename = self._fpath.split("/")[-1]
-        caption_id = filename.split(".")[0]
-        return caption_id
-    def get_data(self):
-        '''returns list of words'''
-        return self._data
-    def __read_raw_text(self):
-        '''read txt file as string'''
-        with open(self._fpath, 'r', encoding='utf-8') as file:
-            text = file.read()
-        return text
-    def __set_data(self, data: List[str]):
-        '''assign list of words to method's _data variable'''
-        self._data = data
