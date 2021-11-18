63b6e1a529d4feaf34eb31454650f23708876f21
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-17 19:26:34 -0500

    done

--- model/make_word2vec_dataset.py
+++
@@ -1,8 +0,0 @@
-'''Make word2vec dataset and save it to disk. 
-In the process, also save cleaned captions and high-frequency cleaned captions to disk.'''
-
-# from src.dataset import Dataset
-from ..config import dataset 
-
-# dataset = Dataset(captions_path="")
-print(dataset.CAPTIONS_PATH)
--- model/train_word2vec.py
+++ model/train_word2vec.py
@@ -1,25 +1,48 @@
-'''Training script for Word2Vec on the dataset's captions'''
-from src.dataset import Dataset
+'''Data Preparation and Training script for Word2Vec'''
+
 from gensim.models import Word2Vec
-import config.word2vec as cfg
+
+from src.dataset import Dataset
+import config.word2vec as wv_cfg
+import config.dataset as ds_cfg 
 
 
-# # prepare dataset for word2vec
-# dataset = Dataset(
-#     captions_path=cfg.CAPTIONS_PATH,
-#     images_path=""
-#     )
-# dataset.read_captions(clean=True)
+def make_dataset(captions_path, captions_checkpoint, word2vec_checkpoint):
+    # make captions dataset
+    dataset = Dataset(
+        captions_path=captions_path,
+        images_path=""
+        )
+    # read captions, clean it and drop words with count < 5
+    dataset.read_captions(clean=True, min_count=5)
+    # save captions dataset
+    dataset.write_captions(captions_checkpoint)
+    # make word2vec dataset
+    dataset.make_word2vec_dataset()
+    # save word2vec dataset
+    dataset.write_word2vec_dataset(word2vec_checkpoint)
+    return dataset
 
-# we've already cleaned the captions
-WORD2VEC_DATASET = dataset.get_word2vec_dataset(min_count=cfg.MIN_COUNT)
+def make_word2vec_model(dataset: Dataset, checkpoint):
+    # get word2vec dataset
+    WORD2VEC_DATASET = dataset.get_word2vec_dataset()
 
-# train word2vec model
-model = Word2Vec(
-    WORD2VEC_DATASET,
-    size=cfg.SIZE,
-    min_count=cfg.MIN_COUNT,
-    workers=cfg.N_CORES,
-    iter=cfg.EPOCHS,
-    window=cfg.WINDOW)
-model.save("word2vec_instacities.model")
+    # train word2vec model
+    model = Word2Vec(
+        WORD2VEC_DATASET,
+        size=wv_cfg.SIZE,
+        min_count=wv_cfg.MIN_COUNT,
+        workers=wv_cfg.N_CORES,
+        iter=wv_cfg.EPOCHS,
+        window=wv_cfg.WINDOW)
+
+    # save the trained model
+    model.save(checkpoint)
+
+
+if __name__ == "__main__":
+    clean_captions_checkpoint = ds_cfg.PROCESSED_CAPTIONS_PATH + "cleaned" + "/" + "clean_captions_min_count_5.pkl"
+    word2vec_dataset_checkpoint = ds_cfg.PROCESSED_CAPTIONS_PATH + "word2vec" + "/" + "word2vec_dataset.pkl"
+    word2vec_model_checkpoint = "word2vec.model"
+    dataset = make_dataset(ds_cfg.CAPTIONS_PATH, clean_captions_checkpoint, word2vec_dataset_checkpoint)
+    make_word2vec_model(dataset, word2vec_model_checkpoint)
