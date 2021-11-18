03cc73ce3c39a3e6c287a909268436dc3dd9df7f
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-17 19:57:55 -0500

    testing model attributes

--- tests/test_word2vec.py
+++ tests/test_word2vec.py
@@ -1,1 +1,26 @@
 '''behavioral and functional tests for the word2vec model'''
+
+from gensim.models import Word2Vec
+
+import config.word2vec as wv_cfg
+
+
+MODEL_PATH = "../model/word2vec.model"
+
+@pytest.fixture
+def model():
+    '''trained word2vec model'''
+    w2v = Word2Vec.load(MODEL_PATH)
+    return w2v
+
+def check_vector_size(model):
+    '''verify size of individual word vector'''
+    assert model.wv.vector_size == wv_cfg.SIZE
+
+def check_window_size(model):
+    '''verify window size used for training'''
+    assert model.window == wv_cfg.WINDOW
+
+def check_epochs(model):
+    '''verify number of epochs model was trained for'''
+    assert model.iter == wv_cfg.EPOCHS
