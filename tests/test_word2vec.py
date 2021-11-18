1540d6208fb08827ab1e2c3f84af4f2e68a71930
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-17 20:03:51 -0500

    functions renamed

--- tests/test_word2vec.py
+++ tests/test_word2vec.py
@@ -14,14 +14,14 @@ def model():
     w2v = Word2Vec.load(MODEL_PATH)
     return w2v
 
-def check_vector_size(model):
+def test_vector_size(model):
     '''verify size of individual word vector'''
     assert model.wv.vector_size == wv_cfg.SIZE
 
-def check_window_size(model):
+def test_window_size(model):
     '''verify window size used for training'''
     assert model.window == wv_cfg.WINDOW
 
-def check_epochs(model):
+def test_epochs(model):
     '''verify number of epochs model was trained for'''
     assert model.iter == wv_cfg.EPOCHS
