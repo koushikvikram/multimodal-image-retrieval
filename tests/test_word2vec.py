ecf2d6437010f1d7619d83e65cede7c95c71e491
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-18 00:29:15 -0500

    all word2vec tests

--- tests/test_word2vec.py
+++ tests/test_word2vec.py
@@ -5,7 +5,7 @@ import pytest
 from gensim.models import Word2Vec
 
 import config.word2vec as wv_cfg
-from tests.case import VOCAB_WORDS, STOP_WORDS
+from tests.word2vec_case import NON_MATCHING_PAIRS, VOCAB_WORDS, STOP_WORDS, SIMILAR_WORDS
 
 
 @pytest.fixture
@@ -39,3 +39,12 @@ def test_word_absence(model, stop_word):
     '''verify the absence of stop words'''
     assert stop_word not in model.wv.vocab
 
+@pytest.mark.parametrize("word1, word2", SIMILAR_WORDS)
+def test_similarity(model, word1, word2):
+    '''test if model scores similarity between words at > 0.5'''
+    assert model.wv.similarity(word1, word2) > 0.5
+
+@pytest.mark.parametrize("words_list, non_matching", NON_MATCHING_PAIRS)
+def test_non_matching(model, words_list, non_matching):
+    '''test if model correctly identifies the non-matching word'''
+    assert model.wv.doesnt_match(words_list) == non_matching
---
+++ tests/word2vec_case.py
@@ -0,0 +1,47 @@
+'''Test Cases'''
+
+VOCAB_WORDS = [
+    "newyork",
+    "nyc",
+    "club",
+    "night",
+    "outdoor",
+    "travel",
+    "fun",
+    "delicious"
+]
+
+STOP_WORDS = [
+    'http',
+    'https',
+    'photo',
+    'picture',
+    'image',
+    'insta',
+    'instagram',
+    'post'
+]
+
+NON_MATCHING_PAIRS = [
+    ("breakfast cereal lunch dinner".split(), "cereal"),
+    ("spoon saturday friday sunday".split(), "spoon"),
+    ("toronto montreal vancouver newyork".split(), "newyork"),
+    ("paint art boy".split(), "boy"),
+    ("hot cool sexy fun".split(), "fun")
+    ("rain snow wind street".split(), "street"),
+    ("travel adventure fun work".split(), "work"),
+    ("guy man dude girl".split(), "girl"),
+    ("park tree forest museum".split(), "museum")
+]
+
+SIMILAR_WORDS = [
+    ("breakfast", "lunch"),
+    ("friday", "sunday"),
+    ("toronto", "montreal"),
+    ("paint", "art"),
+    ("hot", "sexy"),
+    ("rain", "snow"),
+    ("travel", "adventure"),
+    ("guy", "man"),
+    ("tree", "forest")
+]
