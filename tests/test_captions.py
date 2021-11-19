a3aa3cb0dfe66b8d131c57ee3db2d2b3eae6177e
koushikvikram <gridfinsdeploy@gmail.com>
2021-11-18 22:34:38 -0500

    trying indirect parametrization

--- tests/test_captions.py
+++ tests/test_captions.py
@@ -11,29 +11,28 @@ from tests.caption_case import FILE_NAMES, EXCEPTION_FILES, RAW_CAPTIONS, CLEAN_
 def get_raw_captions(filepath):
     '''returns a list of uncleaned words'''
     dataset_path = os.environ.get(TESTING_CAPTIONS_DATASET_PATH)
-    c = Caption(dataset_path + filepath)
-    c.read()
-    return c.get_data()
+    cap = Caption(dataset_path + filepath)
+    cap.read()
+    return cap.get_data()
 
 @pytest.fixture
 def get_clean_captions(filepath):
     '''returns a list of cleaned words'''
     dataset_path = os.environ.get(TESTING_CAPTIONS_DATASET_PATH)
-    c = Caption(dataset_path + filepath)
-    c.read(clean=True)
-    return c.get_data()
+    cap = Caption(dataset_path + filepath)
+    cap.read(clean=True)
+    return cap.get_data()
 
 @pytest.fixture
 def get_caption_id(filepath):
     '''returns the id of the caption file'''
-    c = Caption(filepath)
-    return c.get_id()
+    cap = Caption(filepath)
+    return cap.get_id()
 
 @pytest.mark.parametrize(
-    "get_raw_captions, raw_captions",
+    "filepath, raw_captions",
     list(zip(FILE_NAMES, RAW_CAPTIONS)),
     )
 def test_raw_captions(get_raw_captions, raw_captions):
     '''test if captions are read correctly without cleaning'''
     assert get_raw_captions == raw_captions
-
